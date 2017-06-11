ca80bf7b80a55dab7eb7b5a096f846751195cd4e
Justin Lee <justin.lee007@gmail.com>
2017-06-10 18:32:26 -0700

    Got basic PID working

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,20 +3,30 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
+static const double MAX_STEERING_VALUE = 1.0;
+static const double MIN_STEERING_VALUE = -1.0;
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  double steer = -Kp * p_error - Kd * d_error - Ki * i_error;
+  steer = max(MIN_STEERING_VALUE, steer);
+  steer = min(MAX_STEERING_VALUE, steer);
+  return steer;
 }
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,7 @@ int main() {
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.225, 0.0004, 4);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,12 +59,15 @@ int main() {
           * another PID controller to control the speed!
           */
 
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
           // DEBUG
           cout << "CTE: " << cte << " Steering Value: " << steer_value << endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = 0.4;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           cout << msg << endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
